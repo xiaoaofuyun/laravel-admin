6f273ae8fd041b6e942aa160422d60ef424a4a56
jiejerick <xiaoaofuyun@126.com>
2020-07-23 01:34:42 +0800

    second commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;//添加
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       Schema::defaultStringLength(191);//设置默认长度
+    }
+}
